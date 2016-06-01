7c5c97d4a8ef471b351e94b48b73952be31570d0
Eli Sadoff <snood1205@gmail.com>
2016-06-01 10:37:48 -0400

    Added documentation to Work

--- src/main/java/com/sadofftext/jobapplication/Work.java
+++ src/main/java/com/sadofftext/jobapplication/Work.java
@@ -20,7 +20,7 @@ public class Work{
     /* End date */
     private Date endDate;
     /* Starting salary */
-    private Salary startSalary;
+    private Salary startingSalary;
     /* Ending salary */
     private Salary endingSalary;
     /* Phone number */
@@ -34,7 +34,23 @@ public class Work{
     /* Contact employer? */
     private boolean contact;
 
-    public Work(String company, Name supervisor, int hoursPerWeek, Address address, Date startDate, Date endDate, Salary startSalary,
+    /**
+    * This is the constructor for work.
+    * @param company the company to set
+    * @param supervisor the supervisor to set
+    * @param hoursPerWeek the hours per week to set
+    * @param address the address to set
+    * @param startDate the start date to set
+    * @param endDate the end date to set
+    * @param startingSalary the starting salary to set
+    * @param endingSalary the ending salary to set
+    * @param number the phone number to set
+    * @param jobTitle the job title to set
+    * @param reasonForLeaving the reason for leaving to set
+    * @param duties the duties to set
+    * @param contact set if can be contacted
+    */
+    public Work(String company, Name supervisor, int hoursPerWeek, Address address, Date startDate, Date endDate, Salary startingSalary,
                 Salary endingSalary, Phone number, String jobTitle, String reasonForLeaving, String duties, boolean contact){
         this.company = company;
         this.supervisor = supervisor;
@@ -42,12 +58,220 @@ public class Work{
         this.address = address;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.startSalary = startSalary;
+        this.startingSalary = startingSalary;
         this.endingSalary = endingSalary;
         this.number = number;
         this.jobTitle = jobTitle;
         this.reasonForLeaving = reasonForLeaving;
         this.duties = duties;
+        this.contact = contact;
+    }
+
+    /**
+    * This gets the company.
+    * @return the company
+    */
+    public String getCompany(){
+        return company;
+    }
+
+    /** 
+    * This sets the company.
+    * @param company the company to set
+    */
+    public void setCompany(String company){
+        this.company = company;
+    }
+
+    /**
+    * This gets the supervisor.
+    * @return the supervisor
+    */
+    public Name getSupervisor(){
+        return supervisor;
+    }
+
+    /**
+    * This sets the supervisor.
+    * @param supervisor the supervisor to set
+    */
+    public void setSupervisor(Name supervisor){
+        this.supervisor = supervisor;
+    }
+
+    /**
+    * This gets the hours per week.
+    * @return the hours per week
+    */
+    public int getHoursPerWeek(){
+        return hoursPerWeek;
+    }
+
+    /**
+    * This sets the hours per week.
+    * @param hoursPerWeek the hours per week to set
+    */
+    public void setHoursPerWeek(int hoursPerWeek){
+        this.hoursPerWeek = hoursPerWeek;
+    }
+
+    /**
+    * This gets the address.
+    * @return the address
+    */
+    public Address getAddress(){
+        return address;
+    }
+
+    /**
+    * This sets the address.
+    * @param address the address to set
+    */
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    /**
+    * This gets the start date.
+    * @return the start date
+    */
+    public Date getStartDate(){
+        return startDate;
+    }
+
+    /**
+    * This sets the start date.
+    * @param startDate the start date to set
+    */
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+
+    /**
+    * This gets the end date.
+    * @return the end date
+    */
+    public Date getEndDate(){
+        return endDate;
+    }
+
+    /**
+    * This sets the end date.
+    * @param endDate the end date to set
+    */
+    public void setEndDate(Date endDate){
+        this.endDate = endDate;
+    }
+
+    /**
+    * This gets the starting salary.
+    * @return the starting salary
+    */
+    public Salary getStartingSalary(){
+        return startingSalary;
+    }
+
+    /**
+    * This sets the starting salary.
+    * @param startingSalary the starting salary to set
+    */
+    public void setStartingSalary(Salary startingSalary){
+        this.startingSalary = startingSalary;
+    }
+
+    /**
+    * This gets the ending salary.
+    * @return the ending salary
+    */
+    public Salary getEndingSalary(){
+        return endingSalary;
+    }
+
+    /**
+    * This sets the ending salary.
+    * @param endingSalary the ending salary to set
+    */
+    public void setEndingSalary(Salary endingSalary){
+        this.endingSalary = endingSalary;
+    }
+
+    /**
+    * This gets the phone number.
+    * @return the phone nubmer
+    */
+    public Phone getNumber(){
+        return number;
+    }
+
+    /**
+    * This sets the phone number.
+    * @param number the phone number to set
+    */
+    public void setNumber(Phone number){
+        this.number = number;
+    }
+
+    /**
+    * This gets the job title.
+    * @return the job title
+    */
+    public String getJobTitle(){
+        return jobTitle;
+    }
+
+    /**
+    * This sets the job title.
+    * @param jobTitle the job title to set
+    */
+    public void setJobTitle(String jobTitle){
+        this.jobTitle = jobTitle;
+    }
+
+    /**
+    * This gets the reason for leaving.
+    * @return the reason for leaving
+    */
+    public String getReasonForLeaving(){
+        return reasonForLeaving;
+    }
+
+    /**
+    * This sets the reason for leaving.
+    * @param reasonForLeaving the reason for leaving to set
+    */
+    public void setReasonForLeaving(String reasonForLeaving){
+        this.reasonForLeaving = reasonForLeaving;
+    }
+
+    /**
+    * This gets the duties.
+    * @return the duties
+    */
+    public String getDuties(){
+        return duties;
+    }
+
+    /**
+    * This sets the duties.
+    * @param duties the duties to set
+    */
+    public void setDuties(String duties){
+        this.duties = duties;
+    }
+
+    /**
+    * This gets if can be contacted.
+    * @return if can be contacted
+    */
+    public boolean canContact(){
+        return contact;
+    }
+
+    /**
+    * This sets if can be contacted.
+    * @param contact sets if can be contacted
+    */
+    public void setContact(boolean contact){
         this.contact = contact;
     }
 }
